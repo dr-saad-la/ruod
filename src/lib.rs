c8f424b7f37e06108f6a9fc702da209e6ce58140
Dr.Saad.L <56449463+dr-saad-la@users.noreply.github.com>
2024-08-20 01:59:17 +0100

    Structure the project

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+// lib.rs
+
+pub mod models;  
+pub mod utils;   
+
+
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
---
+++ src/models.rs
@@ -0,0 +1,6 @@
+// models.rs
+
+pub mod iforest;
+pub mod kpca;
+pub mod lof;
+pub mod pca;
