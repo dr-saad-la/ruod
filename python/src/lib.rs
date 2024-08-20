fb029aec6d1e2f6911c11ac99a1eb8b1cb41409d
Dr.Saad.L <56449463+dr-saad-la@users.noreply.github.com>
2024-08-20 06:22:34 +0100

    Update lib.rs module

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use pyo3::prelude::*;
+use ruod::models::pca::PCAOutlierDetector;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[pymodule]
+fn ruod(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<PCAOutlierDetector>()?;
+    Ok(())
 }
